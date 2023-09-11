1351403422a06b5caf5f277f6edd5b3ada639981
rv-13 <r131194@gmail.com>
2023-09-11 15:35:52 +0530

    owner library use cases for property file read and remove boilerplate code

--- src/main/java/com/utils/owner/FrameworkConfig.java
+++ src/main/java/com/utils/owner/FrameworkConfig.java
@@ -3,14 +3,25 @@ package com.utils.owner;
 import org.aeonbits.owner.Config;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+@Config.Sources(value = "file:${user.dir}/src/test/resources/FrameworkConfig.properties")
 public interface FrameworkConfig extends Config {
 
+    //type casted
     String browser();
+
+    //Enum typed
+    @DefaultValue("CHROME")
+    BrowserType browser1();
 
     Long timeout();
 
+    @DefaultValue("SECONDS")
+    TimeUnit value();
+
     boolean takescreenshot();
 
-    List<String> favtools();
+    @Key("favtools")
+    List<String> favstools();
 }
