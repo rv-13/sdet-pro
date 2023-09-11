27b13417c6dd636049dfc012665915621a91f14c
rv-13 <r131194@gmail.com>
2023-09-11 15:36:03 +0530

    owner library use cases for property file read and remove boilerplate code

--- src/test/java/com/tests/owner/Runner.java
+++ src/test/java/com/tests/owner/Runner.java
@@ -1,11 +1,22 @@
 package com.tests.owner;
 
 import com.utils.owner.FrameworkConfig;
+import org.aeonbits.owner.Config;
 import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class Runner {
+
+
     public static void main(String[] args) {
+        WebDriver driver = null;
         FrameworkConfig config = ConfigFactory.create(FrameworkConfig.class);
         System.out.println(config.browser());
+//        driver.manage().timeouts().implicitlyWait(config.timeout(), config.value());
+        config.favstools().forEach(System.out::println);
+        System.out.println(config.timeout());
     }
 }
